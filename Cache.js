ee0ab7fd6725acfd7c516e6cd665acc97f528b3f
thefrostypixel <thefrostypixel@gmail.com>
2026-05-13 19:26:06 +0200

    Add Cache.

---
+++ Cache.js
@@ -0,0 +1,29 @@
+/*
+This file is licensed under CC0 1.0
+(https://creativecommons.org/publicdomain/zero/1.0),
+meaning you can use, modify, and distribute it however you want,
+including commercially, and without including this license.
+*/
+
+globalThis.Cache = class Cache {
+    #cache = Object.create(null);
+    #used = new Set();
+
+    use = (key = "", creator = () => [undefined, () => {}]) => {
+        this.#used.add(key);
+        if (!(key in this.#cache)) {
+            this.#cache[key] = creator();
+        }
+        return this.#cache[key][0];
+    };
+
+    sweep = () => {
+        for (let key in this.#cache) {
+            if (!this.#used.has(key)) {
+                this.#cache[key][1]?.(this.#cache[key][0]);
+                delete this.#cache[key];
+            }
+        }
+        this.#used.clear();
+    };
+};
