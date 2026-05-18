a7f03157e9be11017dc2425e2fced505c3cd7c7b
thefrostypixel <thefrostypixel@gmail.com>
2026-05-18 12:41:40 +0200

    Adjust Box[2|3] matrix generators.
    - Remove Box[2|3].transformTo().
    - Rename Box[2|3].transformTo() to Box[2|3].transformMat3().
    - Add Box2.vertexMat3().

--- Shapes.js
+++ Shapes.js
@@ -815,10 +815,7 @@ globalThis.Box3 = class Box3 {
         return this.xMin <= v.x && v.x <= this.xMax && this.yMin <= v.y && v.y <= this.yMax && this.zMin <= v.z && v.z <= this.zMax;
     }
 
-    transformTo(to = new Box3(0, 1, 0, 1, 0, 1)) {
-        return new Mat3(to.xSize / this.xSize, 0, 0, 0, 0, to.ySize / this.ySize, 0, 0, 0, 0, to.zSize / this.zSize, 0, to.xMin - this.xMin * to.xSize / this.xSize, to.yMin - this.yMin * to.ySize / this.ySize, to.zMin - this.zMin * to.zSize / this.zSize, 1);
-    }
-    transformFrom(from = new Box3(0, 1, 0, 1, 0, 1)) {
+    transformMat3(from = new Box3(0, 1, 0, 1, 0, 1)) {
         return new Mat3(this.xSize / from.xSize, 0, 0, 0, 0, this.ySize / from.ySize, 0, 0, 0, 0, this.zSize / from.zSize, 0, this.xMin - from.xMin * this.xSize / from.xSize, this.yMin - from.yMin * this.ySize / from.ySize, this.zMin - from.zMin * this.zSize / from.zSize, 1);
     }
 };
